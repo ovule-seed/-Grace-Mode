df8a17e3fe609739699b5b38b9fd8337eb9f8500
ovuleseed <110006382+ovule-seed@users.noreply.github.com>
2023-08-07 11:48:18 +0800

    Update 恩情模式.cmd

--- 恩情模式.cmd
+++ 恩情模式.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "flag.jpg" /f
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "PATH/TO/FLAG" /f
 
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 
